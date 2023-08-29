88161ee0ea8bcd3424c8a3db0842241e625551f4
c3s4c <131548301+c3s4c@users.noreply.github.com>
2023-08-29 14:54:27 -0700

    Update replace.js

--- admin/js/replace.js
+++ admin/js/replace.js
@@ -1,1 +1,20 @@
-function replaceData(){}
+function replaceData(){
+    const data = document.getElementById('hello').value;
+    const pass = document.getElementById('pass').value;
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", "https://sub.drscripter32.workers.dev/api/v1/replace");
+    xhr.setRequestHeader("Content-Type", "text/sos-sher");
+    const body = pass+","+data;
+    xhr.onload = () => {
+        if (xhr.readyState == 4 && xhr.status == 201) {
+        console.log(xhr.responseText);
+        document.getElementById('hello').value = '';
+        document.getElementById('pass').value = '';
+    } else {
+        console.log(`Error: ${xhr.status}`);
+    }
+    };
+    xhr.send(body);
+
+
+}
