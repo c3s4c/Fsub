a4cbf6bebfca56c1caf6b29e6b8c2b94c7dcfe93
c3s4c <131548301+c3s4c@users.noreply.github.com>
2023-09-09 11:04:09 -0700

    update

--- admin/js/replace.js
+++ admin/js/replace.js
@@ -1,16 +1,19 @@
 function replaceData(){
     const data = document.getElementById('hello').value;
     const pass = document.getElementById('pass').value;
+    document.getElementById('status').value = 'updating...';
     const xhr = new XMLHttpRequest();
     xhr.open("POST", "https://sub.drscripter32.workers.dev/api/v1/replace");
     xhr.setRequestHeader("Content-Type", "text/sos-sher");
     const body = pass+","+btoa(data);
     xhr.onload = () => {
-        if (xhr.readyState == 4 && xhr.status == 201) {
+        if (xhr.readyState == 4 && xhr.status == 200) {
         console.log(xhr.responseText);
+        document.getElementById('status').value = 'OK : '+xhr.responseText;
         document.getElementById('hello').value = '';
         document.getElementById('pass').value = '';
     } else {
+        document.getElementById('status').value = 'ERR : '+xhr.responseText+' code : '+xhr.status;
         console.log(`Error: ${xhr.status}`);
     }
     };
