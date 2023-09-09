977b3ca937c4b3e4b17df5cff5daba6adb624029
c3s4c <131548301+c3s4c@users.noreply.github.com>
2023-09-09 11:05:49 -0700

    update

--- admin/js/replace.js
+++ admin/js/replace.js
@@ -1,7 +1,7 @@
 function replaceData(){
     const data = document.getElementById('hello').value;
     const pass = document.getElementById('pass').value;
-    document.getElementById('status').value = 'updating...';
+    document.getElementById('status').innerHTML = 'updating...';
     const xhr = new XMLHttpRequest();
     xhr.open("POST", "https://sub.drscripter32.workers.dev/api/v1/replace");
     xhr.setRequestHeader("Content-Type", "text/sos-sher");
@@ -9,11 +9,11 @@ function replaceData(){
     xhr.onload = () => {
         if (xhr.readyState == 4 && xhr.status == 200) {
         console.log(xhr.responseText);
-        document.getElementById('status').value = 'OK : '+xhr.responseText;
+        document.getElementById('status').innerHTML = 'OK : '+xhr.responseText;
         document.getElementById('hello').value = '';
         document.getElementById('pass').value = '';
     } else {
-        document.getElementById('status').value = 'ERR : '+xhr.responseText+' code : '+xhr.status;
+        document.getElementById('status').innerHTML = 'ERR : '+xhr.responseText+' code : '+xhr.status;
         console.log(`Error: ${xhr.status}`);
     }
     };
